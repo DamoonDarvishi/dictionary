69c2defe0a6117a13601135bfd46cb09416c595f
damoondarvishi <damoon.darvishi@gmail.com>
2021-06-07 17:48:08 +0430

    set language

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,33 @@
-import { useState } from "react";
-import SearchWord from "./searchWord";
+import { useState, useEffect } from "react";
+import SearchWord from "./SearchWord";
 import LanguageContext from "../contexts/LanguageContext";
 import ColorContext from "../contexts/ColorContext";
+import axios from "axios";
 
 const App = () => {
-  const [language, setLanguage] = useState("United State");
+  const [language, setLanguage] = useState("en_US");
 
   const onlanguagechange = (name) => {
     setLanguage(name);
+    console.log(language);
   };
+  // https://dictionaryapi.dev/
+  useEffect(() => {
+    const dic = async () => {
+      const { data } = await axios.get(
+        `https://api.dictionaryapi.dev/api/v2/entries/${language}/${"Book"}`
+      );
+      console.log(data);
+    };
+    dic();
+  }, []);
+
   return (
     <div className="ui container">
       <div>
         Select a language:
-        <i
-          className="flag us"
-          onClick={() => onlanguagechange("United State")}
-        />
-        <i
-          className="flag de"
-          onClick={() => onlanguagechange("Deutschland")}
-        />
-        <i className="flag ir" onClick={() => onlanguagechange("Iran")} />
+        <i className="flag us" onClick={() => onlanguagechange("en_US")} />
+        <i className="flag de" onClick={() => onlanguagechange("de")} />
       </div>
       <ColorContext.Provider value="primary">
         <LanguageContext.Provider value={language}>
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,22 +1,13 @@
 import { useEffect } from "react";
 import LanguageContext from "../contexts/LanguageContext";
 import ColorContext from "../contexts/ColorContext";
-import axios from "axios";
 
 const Button = () => {
   const renderSubmit = (value) => {
     return value === "United State" ? "Submit" : "Vorlegen";
   };
 
-  useEffect(() => {
-    const dic = async () => {
-      const { data } = await axios.get(
-        `https://api.dictionaryapi.dev/api/v2/entries/${"en_US"}/${"Book"}`
-      );
-      console.log(data);
-    };
-    dic();
-  }, []);
+
 
   return (
     <ColorContext.Consumer>
